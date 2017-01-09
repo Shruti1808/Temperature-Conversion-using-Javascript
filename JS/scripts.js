8ac47ad3692f0c8d2667b827780da239622c8766
mp and sp <mp@>
2017-01-09 15:22:25 -0800

    convert temperature

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+var Celsius= parseInt(prompt("Enter temperature(in Celsius):"));
+var Fahrenheit= parseInt(prompt("Enter temperature(in Fahrenheit):"));
+
+var Output1 = ((Fahrenheit-32)*0.55);
+var Output2 = ((Celsius+32)*1.8);
+
+var result= alert("your result is " + Output1);
+            alert("your result is "+ Output2);
